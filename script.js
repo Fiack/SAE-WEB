18d216e1864a131ac61d1322f81e22caa6e73fc9
Fiack <118697065+Fiack@users.noreply.github.com>
2023-02-06 12:20:09 +0100

    add

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 const menuHamburger = document.querySelector(".hamburger")
 const navLinks = document.querySelector(".navLinks")
 const croix = document.querySelector(".croix")
+const homeBlanc = document.querySelector(".homeBlanc")
+const homeNoir = document.querySelector(".homeNoir")
 
 menuHamburger.addEventListener('click',()=>{
 navLinks.classList.toggle('mobileMenu')
 menuHamburger.classList.toggle('open')
+homeBlanc.classList.toggle('open')     
 croix.classList.toggle('close')
+homeNoir.classList.toggle('close')
 })
 
 croix.addEventListener('click',()=>{
 navLinks.classList.toggle('mobileMenu')
 menuHamburger.classList.toggle('open')
+homeBlanc.classList.toggle('open')  
 croix.classList.toggle('close')
+homeNoir.classList.toggle('close')
 })
